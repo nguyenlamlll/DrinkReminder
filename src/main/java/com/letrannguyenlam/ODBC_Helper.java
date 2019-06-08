ad275a0e32b3f2121c585894d192a767a4a6d78b
Lam Le <15520405@gm.uit.edu.vn>
2019-06-08 16:30:13 +0700

    Fixing JDBC connection for SQL server

--- src/main/java/com/letrannguyenlam/ODBC_Helper.java
+++ src/main/java/com/letrannguyenlam/ODBC_Helper.java
@@ -5,14 +5,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ODBC_Helper {
-    private static String db_url = "jdbc:sqlserver://CONAN\\LON:;databaseName=DrinkReminder;user=sa;password=Test.123";
-    private static String user = "sa";
-    private static String pw = "Test.123";
+    private static String db_url = "jdbc:sqlserver://.:;databaseName=DrinkReminder;";
+    private static String user = "test_user";
+    private static String password = "Test.123";
     public static void connect() {
         try {
-//            try (Connection con = DriverManager.getConnection("jdbc:sqlserver://.;databaseName=DrinkReminder")){
-//                System.out.println("SQL Connection Established.");
-//            }
+            try (Connection con = DriverManager.getConnection("jdbc:sqlserver://LENOVO-PC\\LAM;databaseName=DrinkReminder;integratedSecurity=True;", user, password)){
+                System.out.println("SQL Connection Established.");
+            }
             try(Connection connection = DriverManager.getConnection("jdbc:mysql://db4free.net:3306/drinkreminder", "testingadmin", "Test.123")) {
                 System.out.println("MySQL Connection Established.");
             }
