eb3a306029bcb531cf9dba6631830c9093dc048d
Lam Le <lam.le@2clicksolutions.com>
2019-06-10 17:27:48 +0700

    Added cloud connection string

--- src/main/java/com/letrannguyenlam/repositories/JDBCDriver.java
+++ src/main/java/com/letrannguyenlam/repositories/JDBCDriver.java
@@ -11,7 +11,7 @@ public class JDBCDriver {
 
     public static void checkConnections() {
         try {
-            try (Connection con = DriverManager.getConnection("jdbc:sqlserver://LENOVO-PC\\LAM;databaseName=DrinkReminder;integratedSecurity=True;", user, password)) {
+            try (Connection con = DriverManager.getConnection("jdbc:sqlserver://.;databaseName=DrinkReminder;integratedSecurity=True;", user, password)) {
                 System.out.println("SQL Connection Established.");
             }
             try (Connection connection = DriverManager.getConnection("jdbc:mysql://db4free.net:3306/drinkreminder", "testingadmin", "Test.123")) {
@@ -23,7 +23,8 @@ public class JDBCDriver {
     }
 
     public Connection createSQLConnection() throws SQLException {
-        Connection connection = DriverManager.getConnection("jdbc:sqlserver://LENOVO-PC\\LAM;databaseName=DrinkReminder;integratedSecurity=True;", user, password);
+//        Connection connection = DriverManager.getConnection("jdbc:sqlserver://EINSTEIN\\EINSTEIN;databaseName=DrinkReminder;integratedSecurity=True;", user, password);
+        Connection connection = DriverManager.getConnection("jdbc:sqlserver://2cs-testing-server.database.windows.net:1433;database=2cs-website-staging;user=TestingAdmin@2cs-testing-server;password=SEhPN6ZMc7yehtF;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
         return connection;
     }
 }
